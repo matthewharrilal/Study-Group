980301c30919e18d7ce1b27369070e182725ca07
matthewharrilal <matthewharrilal@gmail.com>
2017-12-22 00:52:33 -0500

    Making progress on adding the university to Core Data

---
+++ Study-Groups/AddUniversityViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AddUniversityViewController.swift
+//  Study-Groups
+//
+//  Created by Matthew Harrilal on 12/22/17.
+//  Copyright © 2017 Matthew Harrilal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AddUniversityViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
