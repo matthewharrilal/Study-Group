680bd414ea3184fa881966bd82677c6221eee033
matthewharrilal <matthewharrilal@gmail.com>
2018-01-08 11:16:44 -0800

    Refactoring code but login works now to integrate the xollege login authenticator

---
+++ StudyGroupsClient/New Group/Alerts.swift
@@ -0,0 +1,11 @@
+//
+//  Alerts.swift
+//  Study-Groups
+//
+//  Created by Matthew Harrilal on 1/8/18.
+//  Copyright © 2018 Matthew Harrilal. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
